ccccb1aee18af640b7f4c839a60fb8b77b47e609
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-03-21 13:00:14 +0530

    Add initial configuration and runtime integration for fumadocs-mdx

---
+++ .source/index.ts
@@ -0,0 +1,4 @@
+// @ts-nocheck -- skip type checking
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const test = _runtime.doc<typeof _source.test>([]);
---
+++ .source/source.config.mjs
@@ -0,0 +1,9 @@
+// source.config.ts
+import { defineCollections } from "fumadocs-mdx/config";
+var test = defineCollections({
+  type: "doc",
+  dir: "content/docs"
+});
+export {
+  test
+};
