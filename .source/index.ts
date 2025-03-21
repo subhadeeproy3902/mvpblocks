6c3662d967432ce0c4f48a00d7217a20658bd7b6
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-03-21 16:33:26 +0530

    Remove unused components and update ESLint configuration

--- .source/index.ts
+++ .source/index.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/get-started.mdx?collection=docs&hash=1742542328993"
+import * as docs_0 from "../content/docs/get-started.mdx?collection=docs&hash=1742554506815"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
 export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"get-started.mdx","absolutePath":"D:/My github Repositories/mvpblocks/content/docs/get-started.mdx"}, data: docs_0 }]);
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { RootProvider } from "fumadocs-ui/provider";
+import { RootProvider } from 'fumadocs-ui/provider';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
