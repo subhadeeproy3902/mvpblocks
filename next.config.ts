01387ab9383e8dc4c6902c94de60e4643868b7e8
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-03-21 13:00:05 +0530

    Integrate fumadocs-mdx for enhanced documentation support and add related dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import { createMDX } from 'fumadocs-mdx/next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withMDX = createMDX();
+
+/** @type {import('next').NextConfig} */
+const config = {
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withMDX(config);
---
+++ source.config.ts
@@ -0,0 +1,6 @@
+import { defineCollections } from 'fumadocs-mdx/config';
+ 
+export const test = defineCollections({
+  type: 'doc',
+  dir: 'content/docs',
+});
