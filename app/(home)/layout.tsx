86e3e44084fd7a0e410255d4bee936004928445d
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-03-25 12:10:38 +0530

    Add layout component for Docs integration in home directory

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
+import { baseOptions } from '@/app/layout.config';
+import { source } from '@/lib/source';
+import type { ReactNode } from 'react';
+ 
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <DocsLayout
+      {...baseOptions}
+      // the position of navbar
+      nav={{ ...baseOptions.nav }}
+      tree={source.pageTree}
+    >
+      {children}
+    </DocsLayout>
+  );
+}
