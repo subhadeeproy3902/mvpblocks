26da8ee81feb5641354e098d2aead1f6bb47ae67
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-03-25 12:09:57 +0530

    Add initial Fumadocs integration with documentation source configuration

---
+++ .source/index.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck -- skip type checking
+import * as docs_0 from "../content/docs/get-started.mdx?collection=docs&hash=1742883469340"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"get-started.mdx","absolutePath":"D:/My github Repositories/mvpblocks/content/docs/get-started.mdx"}, data: docs_0 }]);
+export const meta = _runtime.meta<typeof _source.meta>([]);
