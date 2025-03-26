4399c5b476b0b31afd8246d6284d6785f4e73d91
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-03-26 10:23:53 +0530

    feat: add globe1 block to registry with dependencies and lazy loading

---
+++ registry/registry-blocks.ts
@@ -0,0 +1,26 @@
+import { siteLink } from "@/config/site";
+import type { Registry } from "@/registry/schema";
+import * as React from "react";
+
+export const blocks: Registry = [
+  {
+    name: "globe1",
+    type: "registry:block",
+    dependencies: ["lucide-react"],
+    registryDependencies: [
+      `${siteLink}/r/globe.json`,
+    ],
+    files: [
+      {
+        path: "@/components/library/creative/globe1.tsx",
+        type: "registry:block",
+      },
+    ],
+    component: React.lazy(
+      () =>
+        import(
+          "../components/library/creative/globe1"
+        )
+    ),
+  }
+];
