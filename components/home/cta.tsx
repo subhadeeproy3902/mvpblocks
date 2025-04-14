131c03e63b6d6b645370e45204ac022482c57971
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-04-14 22:33:17 +0530

    Update CTA component layout for improved spacing and alignment

--- components/home/cta.tsx
+++ components/home/cta.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function CTA() {
   return (
     <section
-      className="flex w-full flex-col items-center justify-center max-w-7xl mx-auto"
+      className="flex w-full flex-col items-center justify-center max-w-7xl mx-auto mb-16"
     >
       <div className="w-full">
         <div className="relative z-20 h-[400px] w-full overflow-hidden rounded-xl border border-border bg-primary shadow-xl md:h-[400px]">
@@ -29,7 +29,7 @@ export default function CTA() {
             <h1 className="max-w-xs text-4xl font-medium tracking-tighter bg-gradient-to-r from-zinc-200/60 via-zinc-50 to-zinc-200/60 text-transparent bg-clip-text text-center md:max-w-xl md:text-7xl">
             Build. Customize. Deploy Quickly.
             </h1>
-            <div className="absolute bottom-10 flex flex-col items-center justify-center gap-2">
+            <div className="absolute bottom-16 flex flex-col items-center justify-center gap-2">
               <Link
                 className="flex h-10 w-fit items-center justify-center rounded-full bg-white px-4 text-sm font-semibold text-black shadow-md"
                 href="/docs/introduction"
