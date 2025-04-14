f85c6294af6606f81b077ced3a7210d45dec256f
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-04-15 01:34:57 +0530

    Refactor Gallery component by removing unused imports and review data

--- components/home/gallery.tsx
+++ components/home/gallery.tsx
@@ -4,9 +4,6 @@ import { Marquee } from "../ui/marquee";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Spotlight } from "../ui/spotlight";
-import LoginForm1 from "../mvpblocks/forms/login-form1";
-import Globe1 from "../mvpblocks/creative/globe1";
-import Globe2 from "../mvpblocks/creative/globe2";
 import { MagicTrailDemo } from "../mvpblocks/trail";
 import { CreativeOTPInputDemo } from "../otp";
 import { AnimatedListDemo } from "../list-demo";
@@ -16,83 +13,6 @@ const space = Geist({
   variable: "--font-carlito",
   weight: "400",
 });
-
-const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
-];
-
-const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
-
-const ReviewCard = ({
-  img,
-  name,
-  username,
-  body,
-}: {
-  img: string;
-  name: string;
-  username: string;
-  body: string;
-}) => {
-  return (
-    <figure
-      className={cn(
-        "relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
-        // light styles
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
-      )}
-    >
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
-        </div>
-      </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
-    </figure>
-  );
-};
 
 export default function Gallery() {
   return (
