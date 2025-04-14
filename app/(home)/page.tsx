1c8d51e9b311f6550fd417eedb141459c2165590
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-04-15 01:29:09 +0530

    Updated UI

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,9 @@
+import EndSlider from "@/components/home/comeagain";
 import CTA from "@/components/home/cta";
+import Faqs from "@/components/home/faq";
 import Features from "@/components/home/features";
 import Footer from "@/components/home/footer";
+import Gallery from "@/components/home/gallery";
 import Hero from "@/components/home/hero";
 import { NavbarDemo } from "@/components/home/navbar";
 import { Testimonials } from "@/components/home/testimonials";
@@ -11,8 +14,11 @@ export default function Homepage() {
       <NavbarDemo />
       <Hero />
       <Features />
+      <Gallery />
       <Testimonials />
       <CTA />
+      <Faqs />
+      <EndSlider />
       <Footer />
     </>
   );
