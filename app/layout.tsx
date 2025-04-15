29096771712f068b8367555923c49ff3333cacf6
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-04-15 12:42:14 +0530

    default dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={`${dm.className}`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
