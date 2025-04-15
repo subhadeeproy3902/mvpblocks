9c7c0d9afe47656fe461dafe0a8fdd8c92f11eb1
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-04-15 18:00:06 +0530

    well well well

--- components/ui/spotlight.tsx
+++ components/ui/spotlight.tsx
@@ -48,7 +48,7 @@ export const Spotlight = ({
           repeatType: "reverse",
           ease: "easeInOut",
         }}
-        className="absolute top-0 left-0 w-screen h-screen z-40 pointer-events-none"
+        className="absolute z-0 top-0 left-0 w-screen h-screen pointer-events-none"
       >
         <div
           style={{
