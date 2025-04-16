477831edf23da398b5ba6e1a1bd9566194b7bb49
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-04-17 04:31:35 +0530

    updated ui

--- app/(home)/about/page.tsx
+++ app/(home)/about/page.tsx
@@ -2,7 +2,7 @@
 
 import Keyboard from "@/components/home/keyboard";
 import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import { Geist } from "next/font/google";
 import { Spotlight } from "@/components/ui/spotlight";
 import Image from "next/image";
@@ -12,8 +12,10 @@ import { Card, CardHeader } from "@/components/ui/card";
 import { BorderBeam } from "@/components/ui/border-beam";
 import CommitActivity from "@/components/about/commit-activity";
 import Codefreq from "@/components/about/code-freq";
-import CTA from "@/components/home/cta";
 import AboutFeaturesSection from "@/components/about/about-features";
+import { useRef } from "react";
+import { Compare } from "@/components/ui/compare";
+import { useTheme } from "next-themes";
 
 const space = Geist({
   subsets: ["latin"],
@@ -22,6 +24,9 @@ const space = Geist({
 });
 
 export default function AboutUsPage() {
+  const {theme} = useTheme();
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.3 });
   return (
     <div className="relative min-h-screen w-full overflow-x-hidden bg-background px-2 py-32 md:px-6">
       <Spotlight />
@@ -71,7 +76,7 @@ export default function AboutUsPage() {
           <div className="absolute left-1/2 top-0 h-px w-3/5 -translate-x-1/2 bg-gradient-to-r from-transparent via-primary/50 to-transparent transition-all ease-in-out"></div>
           <h2
             className={cn(
-              "mb-8 bg-gradient-to-b from-zinc-800 via-foreground to-zinc-700 bg-clip-text text-center text-4xl font-semibold tracking-tighter text-transparent md:text-[54px] md:leading-[60px] pt-12 sm:pt-24 md:pt-32",
+              "mb-8 bg-gradient-to-b from-zinc-800 via-foreground to-zinc-700 bg-clip-text pt-12 text-center text-4xl font-semibold tracking-tighter text-transparent sm:pt-24 md:pt-32 md:text-[54px] md:leading-[60px]",
               space.className,
             )}
           >
@@ -80,8 +85,39 @@ export default function AboutUsPage() {
           <AboutFeaturesSection />
         </div>
 
-        <div className="relative overflow-hidden">
-          
+        <div className="relative md:mb-20">
+          <motion.div
+            ref={ref}
+            initial={{ opacity: 0, y: 50 }}
+            animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+            transition={{ duration: 0.5, delay: 0 }}
+            className="flex max-w-[540px] flex-col items-start justify-start text-left mb-8"
+          >
+            <h2
+              className={cn(
+                "mt-5 bg-gradient-to-r from-foreground/60 via-foreground to-foreground/60 bg-clip-text text-center text-4xl font-semibold tracking-tighter text-transparent dark:from-muted-foreground/55 dark:via-foreground dark:to-muted-foreground/55 md:text-[54px] md:leading-[60px]",
+                space.className,
+              )}
+            >
+              How to use
+            </h2>
+            <p className="mt-5 text-lg text-zinc-500">
+              Just Ctrl C + Ctrl V and you are ready to go. You can use our
+              components in your own projects.
+            </p>
+          </motion.div>
+          <div className="relative xl:flex xl:h-[400px] h-full max-w-5xl mx-auto md:flex-row flex-col items-center justify-start xl:mb-8">
+          <Keyboard />
+          <Compare
+            firstImage={theme === "dark" ? "/abt2.png" : "/abt2-dark.png"}
+            secondImage={theme === "dark" ? "/abt1-dark.png" : "/abt1.png"}
+            firstImageClassName="object-cover object-left-top w-full"
+            secondImageClassname="object-cover object-left-top w-full"
+            className="rounded-[22px] z-0 hidden xl:block -right-32 top-0 absolute md:rounded-lg"
+            slideMode="hover"
+            autoplay={true}
+          />
+          </div>
         </div>
 
         <Card
