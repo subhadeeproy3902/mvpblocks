50a583d83b578095478851f23ca0a5d4ef2e5af6
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-04-22 11:45:30 +0530

    feat: added classic loader

---
+++ components/mvpblocks/basics/loaders/classic-loader.tsx
@@ -0,0 +1,5 @@
+export default function ClassicLoader() {
+  return (
+    <div className="flex h-10 w-10 animate-spin items-center justify-center rounded-full border-4 border-primary border-t-transparent"></div>
+  );
+}
