f1819a9b2e1ca029c20ec601cd357a761aeabcf2
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-04-22 11:46:08 +0530

    feat: added modified classic loader

---
+++ components/mvpblocks/basics/loaders/modified-classic-loader.tsx
@@ -0,0 +1,5 @@
+export default function ModifiedClassicLoader() {
+  return (
+    <div className="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-primary ml-3"></div>
+  );
+}
