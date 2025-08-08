fcc44583269b86bc36dcd4b09c0c5bafb52bcca2
Sinu Kumar <kumarsinu069@gmail.com>
2025-08-08 21:45:57 +0530

    component based pricing is made

--- app/(home)/pricing/page.tsx
+++ app/(home)/pricing/page.tsx
@@ -1,23 +1,9 @@
-'use client';
-
-
-import { Spotlight } from '@/components/ui/spotlight';
-import { Geist } from 'next/font/google';
-import PricingCard from '@/components/pricing/PricingCard';
-
-const space = Geist({
-  subsets: ['latin'],
-  variable: '--font-carlito',
-  weight: '400',
-});
+import PricingSection from '@/components/pricing/PricingSection';
 
 export default function PricingPage() {
   return (
     <div className="relative min-h-screen overflow-hidden px-2 py-32 md:px-6">
-      <Spotlight />
-     <div className="flex justify-center items-center min-h-screen">
-      <PricingCard />
-    </div>
+      <PricingSection />
     </div>
   );
 }
