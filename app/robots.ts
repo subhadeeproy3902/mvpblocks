89c8ec48b11df48814445bcdbed0edf666e7d99c
Subhadeep Roy <subha9.5roy350@gmail.com>
2025-08-13 02:04:55 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -19,6 +19,7 @@ export default function robots(): MetadataRoute.Robots {
         '/privacy',
         '/terms',
         '/showcase',
+        '/404'
       ],
     },
     sitemap: `${siteUrl}/sitemap.xml`,
