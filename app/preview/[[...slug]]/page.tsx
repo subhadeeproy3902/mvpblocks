a19bc8d5024ab149a9fa054af885944d444f0c6e
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-09-04 15:04:41 +0530

    refactor: simplify PreviewPage component by removing error handling and unused registry import feat: update CLI documentation to reflect new color palette options

--- app/preview/[[...slug]]/page.tsx
+++ app/preview/[[...slug]]/page.tsx
@@ -1,6 +1,5 @@
 import { ComponentLoader } from '@/components/preview/component-loader';
 import { notFound } from 'next/navigation';
-import { registry } from '@/registry';
 
 export default async function PreviewPage({
   params,
@@ -11,28 +10,17 @@ export default async function PreviewPage({
   if (!slug.length) return notFound();
   const componentName = slug.join('/');
 
-  try {
-    return (
-      <section className="min-h-screen rounded-md" id="preview">
-        <div className="flex min-h-screen w-full flex-col items-center justify-center overflow-x-hidden">
-          <ComponentLoader
-            name={componentName}
-            hasReTrigger={false}
-            classNameComponentContainer="min-h-screen"
-            previewMode
-          />
-        </div>
-      </section>
-    );
-  } catch (error) {
-    return notFound();
-  }
-}
+  return (
+    <section className="min-h-screen rounded-md" id="preview">
+      <div className="flex min-h-screen w-full flex-col items-center justify-center overflow-x-hidden">
+        <ComponentLoader
+          name={componentName}
+          hasReTrigger={false}
+          classNameComponentContainer="min-h-screen"
+          previewMode
+        />
+      </div>
+    </section>
+  );
 
-export async function generateStaticParams() {
-  const allComponents = registry.map((component) => {
-    return { slug: [component.name] };
-  });
-
-  return allComponents;
 }
