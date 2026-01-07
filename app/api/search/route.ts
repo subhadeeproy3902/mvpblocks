2c72348455f4d61fc009094509feac32de13f11d
subhadeeproy3902 <subha9.5roy350@gmail.com>
2026-01-08 04:43:46 +0530

    lets see

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -6,7 +6,6 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
 
-// it should be cached forever
 export const revalidate = false;
 
-export const { staticGET: GET } = createFromSource(source);
+export const { GET } = createFromSource(source);
