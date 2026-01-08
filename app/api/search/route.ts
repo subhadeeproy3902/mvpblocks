fb302e4df019aae4589d4405328ca2fed5baff7e
subhadeeproy3902 <subha9.5roy350@gmail.com>
2026-01-08 12:02:11 +0530

    fix

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -6,7 +6,7 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
 
-export const runtime = 'nodejs';
-export const dynamic = 'force-dynamic';
+// it should be cached forever
+export const revalidate = false;
 
-export const { GET } = createFromSource(source);
+export const { staticGET: GET } = createFromSource(source);
